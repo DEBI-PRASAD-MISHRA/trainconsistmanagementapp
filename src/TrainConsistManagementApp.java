247a2fae4af029e9ca6aafeddf19b9e9d187a556
DEBI-PRASAD-MISHRA <joojooofpuri@gmail.com>
2026-04-06 20:56:31 +0530

    Base application setup

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class InvalidCapacityException extends Exception {
+    public InvalidCapacityException(String message) {
+        super(message);
+    }
+}
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        // Initial setup
+    }
+}

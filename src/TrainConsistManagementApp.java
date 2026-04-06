adbd54ca735ea1603f30d6afc86b5e1ebd5f2f71
DEBI-PRASAD-MISHRA <joojooofpuri@gmail.com>
2026-04-06 20:56:35 +0530

    UC1: Implementation

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -9,6 +9,12 @@ class InvalidCapacityException extends Exception {
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        // Initial setup
+        // UC1 — Initialize Train and Display Consist Summary
+        System.out.println("--- UC1: Initialize Train ---");
+        String trainId = "EXP-101";
+        String trainName = "Rajdhani Express";
+        int totalBogieCount = 0;
+        System.out.println("Train ID: " + trainId + ", Name: " + trainName + ", Bogies: " + totalBogieCount);
+
     }
 }
